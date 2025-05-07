493c55b548bb365c56870d4dcc2c5a914c869fad
君惜 <xinlei3166@126.com>
2025-05-07 18:54:39 +0800

    feat: Migrate axios to uni.request

--- src/api/data.ts
+++ src/api/data.ts
@@ -3,6 +3,7 @@ import { requests } from './base'
 import * as mock from '@/mock'
 
 export function getList(config?: Config): Promise<Response> {
+  // return requests.get('http://jsonplaceholder.typicode.com/posts', config)
   // return requests.get('/api/mock/data/list', config)
   return mock.getList()
 }
