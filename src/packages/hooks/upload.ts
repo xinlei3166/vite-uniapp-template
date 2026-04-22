c716631e70a1effcfc89090c713e533e6fb8e2fd
君惜 <xinlei3166@126.com>
2026-04-22 17:16:05 +0800

    feat: 实现文件上传进度模拟，优化上传逻辑

--- src/packages/hooks/upload.ts
+++ src/packages/hooks/upload.ts
@@ -52,26 +52,21 @@ export function useUpload({ maxCount, maxSize, accept, upload = true }: UploadCo
     fileList.value = newFileList
   }
 
-  const onUpload = () => {
-    const formData = new FormData()
-    fileList.value?.forEach((file: UploadFile) => {
-      formData.append('files[]', file as any)
+  const fakeRequestMethod: any = async (file: any) => {
+    return new Promise(resolve => {
+      let percent = 0
+      const timer = setInterval(() => {
+        percent += 30
+        file.percent = percent // 更新进度条
+        if (percent >= 100) {
+          clearInterval(timer)
+          resolve({
+            status: 'success',
+            response: { url: '' } // response 必须有，不然报错
+          })
+        }
+      }, 200)
     })
-    uploading.value = true
-
-    // request('https://www.mocky.io/v2/5cc8019d300000980a055e76', {
-    //   method: 'post',
-    //   data: formData
-    // })
-    //   .then(() => {
-    //     fileList.value = []
-    //     uploading.value = false
-    //     uni.showToast({ title: '上传成功' })
-    //   })
-    //   .catch(() => {
-    //     uploading.value = false
-    //     uni.showToast({ title: '上传失败' })
-    //   })
   }
 
   const createRequestMethod = (
@@ -100,6 +95,7 @@ export function useUpload({ maxCount, maxSize, accept, upload = true }: UploadCo
 
       // 3. 执行请求
       try {
+        uploading.value = true
         const res = await uploadFile(formData, {
           headers: { 'Content-Type': ContentTypeEnum.FormData }
           // onUploadProgress: (event: any) => {
@@ -130,6 +126,8 @@ export function useUpload({ maxCount, maxSize, accept, upload = true }: UploadCo
         }
         console.error('requestMethod error', err, failResponse)
         return failResponse
+      } finally {
+        uploading.value = false
       }
     }
   }
@@ -151,7 +149,8 @@ export function useUpload({ maxCount, maxSize, accept, upload = true }: UploadCo
     uploading,
     onRemove,
     beforeUpload,
-    onUpload,
+    fakeRequestMethod,
+    onUpload: requestMethod,
     requestMethod,
     createRequestMethod,
     onChange
